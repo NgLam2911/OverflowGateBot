0609b2d6a629d92b0f27b9a762b7b13d878b0152
Shar <66898063+VN-Shar@users.noreply.github.com>
2022-09-29 23:18:24 +0700

    fix

--- OverflowGateBot/src/OverflowGateBot/CommandHandler.java
+++ OverflowGateBot/src/OverflowGateBot/CommandHandler.java
@@ -27,6 +27,7 @@ public class CommandHandler {
                 try {
                     serverStatus.save();
                     userHandler.save();
+                    guildConfigHandler.save();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
